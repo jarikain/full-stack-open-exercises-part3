1f95221ed68627bc8e214c3815b06ca39b67a37d
Jari Kainulainen <128816881+jarikain@users.noreply.github.com>
2023-11-09 16:14:19 +0200

    Add exercise 3.2

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ For this part of the course, it is recommended to use separate repository. My ma
 
 ## Completed exercises:
 - [x] 3.1
-- [ ] 3.2
+- [x] 3.2
 - [ ] 3.3
 - [ ] 3.4
 - [ ] 3.5
--- index.js
+++ index.js
@@ -31,6 +31,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`)
 })
